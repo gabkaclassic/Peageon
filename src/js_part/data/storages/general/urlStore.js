2feb24219aa9dd092ffcf6df7f9597239121e415
Errodion <kuzminrd44@gmail.com>
2023-05-23 09:24:02 +0300

    Merging with devRodion

--- src/js_part/data/storages/general/urlStore.js
+++ src/js_part/data/storages/general/urlStore.js
@@ -1,8 +1,8 @@
 export default {
     state: {
         authoadizationBaseUrl: 'https://1462419-ck65966.tw1.ru:5004/',
-        gitoadBaseUrl: 'https://localhost:5001/',
-        // gitoadBaseUrl: 'https://1462419-ck65966.tw1.ru:5001/',
+        // gitoadBaseUrl: 'https://localhost:5001/',
+        gitoadBaseUrl: 'https://1462419-ck65966.tw1.ru:5001/',
 
 
     },
